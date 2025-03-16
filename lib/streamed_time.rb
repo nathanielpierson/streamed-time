7a7fa3ebdabb6fcbd76c6bf84af2813924a23e7d
nathanielpierson <nathanielepierson@gmail.com>
2025-03-16 14:27:06 -0400

    reverted to original project because nothing works

--- lib/streamed_time.rb
+++ lib/streamed_time.rb
@@ -1,34 +1,35 @@
 require 'http'
+require './base'
 
-class Streamed_time
-  def self.hiy
-    puts "hello world!"
-  end
+class Streamed_time < Base
+
+
   def self.statement(streamer)
-    request = HTTP.headers(:client_id => "pnyd2wx6lmfrsubv7jd2rmakek0g7h").auth("Bearer v3y6hy744layrep52r2hlhdj7nmgv7").get("https://api.twitch.tv/helix/search/channels?query=#{streamer}&live_only=false")
-    get_info = request.parse
-    streamer_live = false
-    real_streamer = false
-    x = 0
-
-# cycles through the search results until the correct streamer comes up, then exit loop
-    while real_streamer == false
-      temp = get_info["data"][x]
-      if temp["display_name"] == "#{streamer}"
-        real_streamer = true
-        # execute additional code later if streamer is currently live
-        if temp["is_live"] == true
-          streamer_live = true
-          game = temp["game_name"]
-          time = temp["started_at"]
+    def self.init(streamer)
+      request = HTTP.headers(:client_id => "pnyd2wx6lmfrsubv7jd2rmakek0g7h").auth("Bearer v3y6hy744layrep52r2hlhdj7nmgv7").get("https://api.twitch.tv/helix/search/channels?query=#{streamer}&live_only=false")
+      get_info = request.parse
+      streamer_live = false
+      real_streamer = false
+      x = 0
+  
+  # cycles through the search results until the correct streamer comes up, then exit loop
+      while real_streamer == false
+        temp = get_info["data"][x]
+        if temp["display_name"] == "#{streamer}"
+          real_streamer = true
+          # execute additional code later if streamer is currently live
+          if temp["is_live"] == true
+            streamer_live = true
+            game = temp["game_name"]
+            time = temp["started_at"]
+          else
+            streamer_live = false
+          end
         else
-          streamer_live = false
+          x += 1
         end
-      else
-        x += 1
       end
-    end
-
+    end 
     if streamer_live == true
       d = DateTime.now
       current_day = (d.strftime("%D")[3] + d.strftime("%D")[4]).to_i
@@ -116,4 +117,4 @@ class Streamed_time
     end
   end
 end
-Streamed_time.statement("itsbeeve")
+Streamed_time.statement("ikori_mario")
--- streamed_time.gemspec
+++ streamed_time.gemspec
@@ -1,9 +1,9 @@
 Gem::Specification.new do |s|
   s.name = "streamed_time"
-  s.version = "0.0.3"
-  s.files = ["lib/streamed_time.rb"]
+  s.version = "0.0.5"
+  s.files = ["lib/streamed_time.rb", "lib/base.rb"]
   s.description = "testing hello world before building gem"
-  s.summary = "how long has Twitch streamer been live"
+  s.summary = "testing update"
   s.authors = ["Nathaniel Pierson"]
   s.license = nil
 end
